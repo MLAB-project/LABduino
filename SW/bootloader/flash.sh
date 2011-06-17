2828458a7d3d5602c1951b7421bdbc7b7bf6581d
Jakub Kákona <kaklik@mlab.cz>
2011-06-17 14:19:00 +0000

    oprava dialogu

--- SW/bootloader/flash.sh
+++ SW/bootloader/flash.sh
@@ -1,7 +1,7 @@
 # Linux Shell Script
 # ATprogISPUSB02A fash script
 # Uses DAPA (Direct Parallel Access) Programmer
-read -p "WARNING: This procedure enables external clock. Make sure a crystal is connected to the MCU and connect a programmer to the ATprogISPUSB02A"
+read -p "WARNING: This procedure enables external clock. Make sure if a 16MHz crystal is soldered and connect the MCU module to the programmer ATprogISPUSB02A"
 avrdude -F -p atmega328P -P /dev/ttyUSB0 -c stk500v2 -B 50 -v -U efuse:w:0x05:m -U lfuse:w:0xff:m -U hfuse:w:0xda:m 
 avrdude -F -p atmega328P -P /dev/ttyUSB0 -c stk500v2 -B 50 -U flash:w:ATmegaBOOT_168_atmega328.hex:a 
 echo "If none errors above - Please connect the USB232R01B module to bootload the LABduino via Arduino software."
