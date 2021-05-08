f8b69c789e5d24f130c81246ca40534273cb005b
kaklik <kaklik@f55caa52-0715-0410-8694-db4083128028>
2021-05-08 21:29:00 +0000

    Disable brownout reset in efuse.

--- Designs/LABduino/SW/bootloader/flash.sh
+++ Designs/LABduino/SW/bootloader/flash.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # Linux Shell Script
 # ATprogISPUSB02A fash script
 # Uses DAPA (Direct Parallel Access) Programmer
-read -p "WARNING: This procedure enables external clock. Make sure if a 16MHz crystal is installed on MCU module with ATmega328 and connect the MCU module to the programmer ATprogISPUSB02A"
-avrdude -F -p atmega328P -P $1 -c stk500v2 -B 50 -v -U efuse:w:0x05:m -U lfuse:w:0xff:m -U hfuse:w:0xda:m 
-avrdude -F -p atmega328P -P $1 -c stk500v2 -B 50 -U flash:w:ATmegaBOOT_168_atmega328.hex:a 
+read -p "WARNING: This procedure enables external clock. Make sure if a 16MHz crystal is installed on MCU module with ATmega328 and connect the MCU module to the programmer ATprogISPUSB02A" -n1 -s
+avrdude -F -p atmega328P -P $1 -c stk500v2 -B 50 -v -U efuse:w:0xff:m -U lfuse:w:0xff:m -U hfuse:w:0xda:m
+avrdude -F -p atmega328P -P $1 -c stk500v2 -B 50 -U flash:w:ATmegaBOOT_168_atmega328.hex:a
 echo "If none errors above - Please connect the USB232R01B module to bootload the LABduino via Arduino software."
